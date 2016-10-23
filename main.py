2eeb04e6b98e78b3dac6d5006f41ec9c3dfde6dd
jin09 <supergautam995@gmail.com>
2016-10-24 01:11:54 +0530

    Authentication cookies , signup modified, added login page, logout handler

--- main.py
+++ main.py
@@ -14,15 +14,63 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import re
+import random
+import string
 import webapp2
 import jinja2
 import os
+import re
+import hmac
 from google.appengine.ext import db
+import hashlib
+
+
+SECRET = "bLah!bLaH|SecreT|Key!!"
+
 
 jinja_env = jinja2.Environment(autoescape=True,
                                loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
+
+def make_salt():
+    return ''.join(random.choice(string.letters) for x in xrange(5))
+
+
+def make_user_cookie(user_val):
+    return "%s|%s" % (str(user_val),str(hmac.new(SECRET,str(user_val)).hexdigest()))
+
+
+def check_valid_cookie(test_cookie):
+    user_val = test_cookie.split('|')[0]
+    if make_user_cookie(user_val) == test_cookie:
+        return True
+    else:
+        return False
+
+
+def create_pass_hash(pwd,name):
+    salt = make_salt()
+    h = hashlib.sha256(name+pwd+salt).hexdigest()
+    return "%s,%s" % (h,salt)
+
+
+def check_valid_pass(pass_val,pass_hash,username):
+    h = hashlib.sha256(username+pass_val+pass_hash.split(',')[1]).hexdigest()
+    if h == pass_hash.split(',')[0]:
+        return True
+    else:
+        return False
+
+
+class user_table(db.Model):
+    name = db.StringProperty(required=True)
+    pw_hash = db.StringProperty(required=True)
+    email = db.StringProperty
+
+    @classmethod
+    def by_name(cls,name):
+        u = user_table.all().filter('name=', name).get()
+        return u
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 
@@ -116,7 +164,13 @@ class NewPostHandler(Handler):
         self.render("newpost.html", error=error, content_value=content, title_value=title)
 
     def get(self):
-        self.render_form()
+        user_id = self.request.cookies.get("user_id", "0")
+        if user_id == "0":
+            self.redirect('/')
+        if check_valid_cookie(user_id) == True:
+            self.render_form()
+        else:
+            self.redirect('/login')
 
     def post(self):
         title = self.request.get("title")
@@ -133,8 +187,14 @@ class NewPostHandler(Handler):
 
 
 class SignupHandler(Handler):
+    def render_front(self, username_value="", username_error="", pass_value="", pass_error="", verifypass_value="",
+                     verifypass_error="", email_value="", email_error=""):
+        self.render("signup.html", username_value=username_value, username_error=username_error,
+                    pass_value=pass_value, pass_error=pass_error, verifypass_value=verifypass_value,
+                    verifypass_error=verifypass_error, email_value=email_value, email_error=email_error)
+
     def get(self):
-        self.render("signup.html")
+        self.render_front()
 
     def post(self):
         is_error = False
@@ -146,15 +206,24 @@ class SignupHandler(Handler):
         pass_error = ""
         verifypass_error = ""
         email_error = ""
-        if (username):
-            if (valid_username(username) == False):
+        if username:
+            if valid_username(username) == False:
                 is_error = True
-                username_error = "Thats not a valid username"
-        if (passw):
-            if (valid_pass(passw) == False):
+                username_error = "That's not a valid username"
+
+        if is_error == False:
+            all = db.GqlQuery("select * from user_table")
+            for i in all:
+                if i.name == username:
+                    is_error = True
+                    username_error = "username already exists !!"
+
+        if passw:
+            if valid_pass(passw) == False:
                 is_error = True
-                pass_error = "Invalid Pass"
-        if (valid_pass(passw) == True):
+                pass_error = "Your password isn't strong enough !!"
+
+        if valid_pass(passw) == True:
             if (verify_pass != passw):
                 is_error = True
                 verifypass_error = "Passwords Don't match"
@@ -165,11 +234,89 @@ class SignupHandler(Handler):
                 email_error = "Invalid Email address"
 
         if (is_error == True):
-            self.render("signup.html", username_value=username, username_error=username_error, pass_value=passw,
-                        pass_error=pass_error, verifypass_value=verify_pass,
-                        verifypass_error=verifypass_error, email_value=email, email_error=email_error)
+            self.render_front(username, username_error, passw,
+                              pass_error, verify_pass,
+                              verifypass_error, email, email_error)
         else:
-            self.redirect("/verifed?username=" + username)
+            pass_hash = create_pass_hash(passw, username)
+            row = user_table(name=username, pw_hash=pass_hash, email=email)
+            row.put()
+
+            id = row.key().id()
+            cookie = make_user_cookie(id)
+            self.response.headers.add_header('Set-Cookie', "user_id=%s" % str(cookie))
+            self.redirect('/welcome')
+
+
+class WelcomeHandler(Handler):
+    def get(self):
+        user_id = self.request.cookies.get("user_id","0")
+        if user_id == "0":
+            self.redirect('/')
+        if check_valid_cookie(user_id) == True:
+            id = user_id.split('|')[0]
+            key = db.Key.from_path('user_table', int(id))
+            row = db.get(key)
+            self.render("welcome.html", row=row)
+        else:
+            self.redirect('/login')
+
+
+class LoginHandler(Handler):
+    def render_form(self,username_value="", username_error="", pass_val="", pass_error=""):
+        self.render("login.html", username_value=username_value, username_error=username_error, pass_value=pass_val, pass_error=pass_error)
+
+    def get(self):
+        self.render_form()
+
+    def post(self):
+        username = self.request.get("username")
+        passw = self.request.get("passw")
+        username_error = ""
+        pass_error = ""
+        is_error = False
+        all = db.GqlQuery("select * from user_table")
+        test_global = all[0]
+
+        if username:
+            all = db.GqlQuery("select * from user_table")
+            is_valid_name = False
+            for i in all:
+                if i.name == username:
+                    test_global = i
+                    is_valid_name = True
+                    break
+            if is_valid_name == False:
+                username_error = "Invalid Username"
+                is_error = True
+        else:
+            is_error = True
+            username_error = "Invalid Username"
+
+        if is_error == True:
+            self.render_form(username,username_error,passw)
+
+        else:
+            if passw:
+                actual_pass = test_global.pw_hash
+                ans = check_valid_pass(passw,actual_pass,username)
+                if ans == False:
+                    pass_error = "Password does'nt match !!"
+                    is_error = True
+
+            if is_error == True:
+                self.render_form(username,username_error,passw,pass_error)
+            else:
+                id = test_global.key().id()
+                cookie = make_user_cookie(id)
+                self.response.headers.add_header('Set-Cookie', "user_id=%s" % str(cookie))
+                self.redirect('/welcome')
+
+
+class LogoutHandler(Handler):
+    def get(self):
+        self.response.headers.add_header('Set-Cookie', "user_id=%s" % str(""))
+        self.redirect('/')
 
 
 class VerifiedHandler(Handler):
@@ -207,5 +354,8 @@ app = webapp2.WSGIApplication([
     ('/verifed', VerifiedHandler),
     ('/asciichan', AsciiChanHandler),
     ('/([0-9]+)', PermalinkHandler),
-    ('/newpost', NewPostHandler)
+    ('/newpost', NewPostHandler),
+    ('/welcome', WelcomeHandler),
+    ('/login', LoginHandler),
+    ('/logout', LogoutHandler)
 ], debug=True)
